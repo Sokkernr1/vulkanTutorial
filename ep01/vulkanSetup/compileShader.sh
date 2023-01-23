1331e615961c62ef6b92d08cac99b1e52b671500
Paul Sohns <paul.sohns@web.de>
2023-01-23 17:55:03 +0100

    I dont know whats happening...

--- ep01/vulkanSetup/compileShader.sh
+++
@@ -1,2 +0,0 @@
-/Users/paul.sohns/VulkanSDK/1.3.236.0/macOS/bin/glslc src/shader/simple_shader.vert -o shader/simple_shader.vert.spv
-/Users/paul.sohns/VulkanSDK/1.3.236.0/macOS/bin/glslc src/shader/simple_shader.frag -o shader/simple_shader.frag.spv
