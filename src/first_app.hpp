c102878cd1acf02d9b4a7e643b877fb4bde21eaf
Paul Sohns <paul.sohns@web.de>
2023-01-30 19:26:44 +0100

    Read shader files

--- src/first_app.hpp
+++ src/first_app.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "lve_window.hpp"
+#include "lve_pipeline.hpp"
 
 namespace lve {
 
@@ -12,6 +13,7 @@ namespace lve {
             void run();
         private:
             lveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+            LvePipeline lvePipeline{"../shader/simple_shader.vert.spv", "../shader/simple_shader.frag.spv"};
     };
 
 }
